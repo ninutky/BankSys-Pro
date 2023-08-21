e27f1745295822d3e6575fab8b382fa8a6a8e404
Nagyeong Park <zzum0701@naver.com>
2023-08-21 10:30:16 +0900

    first commit

---
+++ src/main/java/kr/hs/study/BankSysPro/BankSysProApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.BankSysPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSysProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSysProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/study/BankSysPro/BankSysProApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.BankSysPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSysProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
